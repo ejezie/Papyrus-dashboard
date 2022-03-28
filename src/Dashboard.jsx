3a1a1c16bf20f2666b68f11c614ff9017391cf2e
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-03-28 19:51:23 +0100

    added map

---
+++ src/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d126844.06348606381!2d3.3718272!3d6.537216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sng!4v1648493124236!5m2!1sen!2sng"></iframe>
+    </div>
+  );
+}
+
+export default Dashboard;
